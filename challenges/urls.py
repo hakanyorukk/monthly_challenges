d690e9e51616013739526d9c9c33af8be6cdcb80
hakanyorukk <hakanyorukomer@hotmail.com>
2023-08-17 16:18:52 +0300

    Initial commit

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,5 +5,6 @@ from .views import DetailView
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="index"), #starting page
-    path("months/<int:pk>", views.MonthlyChallengeDetailView.as_view(), name="month_challenge")
+    path("/months/<int:pk>", views.MonthlyChallengeDetailView.as_view(), name="month_challenge"),
+    path("accept-challenge", views.AcceptChallenge.as_view(), name="accept-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -15,4 +15,38 @@ class StartingPageView(ListView):
 class MonthlyChallengeDetailView(DetailView):
     template_name = "challenges/challenge.html"
     model = Months
-    
+
+class AcceptChallenge(View):
+    def get(self,requset):
+        accepted_challenges = requset.session.get("accepted_challenges")
+        context = {}
+
+        if accepted_challenges is None:
+            context["accepts"] = []
+            context["has_accept"] = False
+
+        else:
+            accepts = Months.objects.filter(id__in=accepted_challenges)
+            context["accepts"] = accepts
+            context["has_accept"] = True
+        return render(requset, "", context)
+
+
+    def post(self,requset):
+        accepted_challenges = requset.session.get("accepted_challenges")
+
+        if accepted_challenges is None:
+            accepted_challenges = []
+        
+        accept_id = requset.POST["accept_id"]
+
+        if accept_id not in accepted_challenges:
+            print(accept_id)
+            print(accepted_challenges)
+            accepted_challenges.append(accept_id)
+
+        else:
+            accepted_challenges.remove(accept_id)
+
+        requset.session["accepted_challenges"] = accepted_challenges
+        return HttpResponseRedirect("/")
