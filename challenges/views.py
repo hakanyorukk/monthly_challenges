dbd89ae2634a91ad661cdd3d57c024c486f1f567
hakanyorukk <hakanyorukomer@hotmail.com>
2023-08-18 16:30:12 +0300

    Added Session

--- challenges/views.py
+++ challenges/views.py
@@ -13,24 +13,52 @@ class StartingPageView(ListView):
     template_name = "challenges/index.html"
     context_object_name = "months"
 
-class MonthlyChallengeDetailView(DetailView):
-    template_name = "challenges/challenge.html"
-    model = Months
-    context_object_name="month"
+# class MonthlyChallengeDetailView(DetailView):
+#     template_name = "challenges/challenge.html"
+#     model = Months
+#     context_object_name="month"
+class MonthlyChallengeDetailView(View): 
+    def is_accepted_challenges(self, request, month_id):
+        accepted_challenges = request.session.get("accepted_challenges")
 
+        if accepted_challenges is not None:
+            is_accepted_challenges = month_id in accepted_challenges
+        else:
+            is_accepted_challenges=False
+
+        return is_accepted_challenges
+    
+    def get(self,request, slug):
+        month = Months.objects.get(slug=slug)
+        context = {
+            "month":month,
+            "save_challenge":self.is_accepted_challenges(request, month.id)
+        }
+        return render(request, "challenges/challenge.html", context)
+    
+    
+    def post(self, request, slug):  
+        month = Months.objects.get(slug=slug)
+        context = {
+            "month":month,
+            "save_challenge":self.is_accepted_challenges(request, month.id)
+        }
+        return render(request, "challenges/challenge.html", context)
+    
 class AcceptChallenge(View):
     def get(self,request):
         accepted_challenges = request.session.get("accepted_challenges")
         context = {}
 
         if accepted_challenges is None:
-            context["accepts"] = []
+            context["months"] = []
             context["has_accept"] = False
 
         else:
-            accepts = Months.objects.filter(id__in=accepted_challenges)
-            context["accepts"] = accepts
+            month = Months.objects.filter(id__in=accepted_challenges)
+            context["months"] = month
             context["has_accept"] = True
+            context["save-challenge"] = True
         return render(request, "challenges/accept-challenge.html", context)
 
 
@@ -41,9 +69,11 @@ class AcceptChallenge(View):
             accepted_challenges = []
         
         month_id = int(request.POST["month_id"])
+        print(month_id)
         if month_id not in accepted_challenges:
             accepted_challenges.append(month_id)
-            print(month_id)
+        else:
+            accepted_challenges.remove(month_id)
 
         request.session["accepted_challenges"] = accepted_challenges
         return HttpResponseRedirect("/")
